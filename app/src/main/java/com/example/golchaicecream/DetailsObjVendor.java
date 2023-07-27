2d3bb4c1b8200b4df9f5063a47808e16a5ca61c2
kush-raj-nit061 <kushpvt1@gmail.com>
2023-07-27 20:39:21 +0530

    UserAdmin sepration remains

---
+++ app/src/main/java/com/example/golchaicecream/DetailsObjVendor.java
@@ -0,0 +1,607 @@
+package com.example.golchaicecream;
+
+public class DetailsObjVendor {
+
+    int kacchaQtyDepart;
+    int litchiQtyDepart;
+    int strawQtyDepart;
+    int colaQtyDepart;
+    int pineQtyDepart;
+    int orangeQtyDepart;
+    int mangoQtyDepart;
+    int cupSQtyDepart;
+    int cupBQtyDepart;
+    int chocoSQtyDepart;
+    int chocoBQtyDepart;
+    int matkaQtyDepart;
+    int coneSQtyDepart;
+    int coneBQtyDepart;
+    int nuttyQtyDepart;
+    int keshaerQtyDepart;
+    int bonanzaQtyDepart;
+    int familyQtyDepart;
+    int family2QtyDepart;
+
+    public int getKacchaQtyDepart() {
+        return kacchaQtyDepart;
+    }
+
+    public void setKacchaQtyDepart(int kacchaQtyDepart) {
+        this.kacchaQtyDepart = kacchaQtyDepart;
+    }
+
+    public int getLitchiQtyDepart() {
+        return litchiQtyDepart;
+    }
+
+    public void setLitchiQtyDepart(int litchiQtyDepart) {
+        this.litchiQtyDepart = litchiQtyDepart;
+    }
+
+    public int getStrawQtyDepart() {
+        return strawQtyDepart;
+    }
+
+    public void setStrawQtyDepart(int strawQtyDepart) {
+        this.strawQtyDepart = strawQtyDepart;
+    }
+
+    public int getColaQtyDepart() {
+        return colaQtyDepart;
+    }
+
+    public void setColaQtyDepart(int colaQtyDepart) {
+        this.colaQtyDepart = colaQtyDepart;
+    }
+
+    public int getPineQtyDepart() {
+        return pineQtyDepart;
+    }
+
+    public void setPineQtyDepart(int pineQtyDepart) {
+        this.pineQtyDepart = pineQtyDepart;
+    }
+
+    public int getOrangeQtyDepart() {
+        return orangeQtyDepart;
+    }
+
+    public void setOrangeQtyDepart(int orangeQtyDepart) {
+        this.orangeQtyDepart = orangeQtyDepart;
+    }
+
+    public int getMangoQtyDepart() {
+        return mangoQtyDepart;
+    }
+
+    public void setMangoQtyDepart(int mangoQtyDepart) {
+        this.mangoQtyDepart = mangoQtyDepart;
+    }
+
+    public int getCupSQtyDepart() {
+        return cupSQtyDepart;
+    }
+
+    public void setCupSQtyDepart(int cupSQtyDepart) {
+        this.cupSQtyDepart = cupSQtyDepart;
+    }
+
+    public int getCupBQtyDepart() {
+        return cupBQtyDepart;
+    }
+
+    public void setCupBQtyDepart(int cupBQtyDepart) {
+        this.cupBQtyDepart = cupBQtyDepart;
+    }
+
+    public int getChocoSQtyDepart() {
+        return chocoSQtyDepart;
+    }
+
+    public void setChocoSQtyDepart(int chocoSQtyDepart) {
+        this.chocoSQtyDepart = chocoSQtyDepart;
+    }
+
+    public int getChocoBQtyDepart() {
+        return chocoBQtyDepart;
+    }
+
+    public void setChocoBQtyDepart(int chocoBQtyDepart) {
+        this.chocoBQtyDepart = chocoBQtyDepart;
+    }
+
+    public int getMatkaQtyDepart() {
+        return matkaQtyDepart;
+    }
+
+    public void setMatkaQtyDepart(int matkaQtyDepart) {
+        this.matkaQtyDepart = matkaQtyDepart;
+    }
+
+    public int getConeSQtyDepart() {
+        return coneSQtyDepart;
+    }
+
+    public void setConeSQtyDepart(int coneSQtyDepart) {
+        this.coneSQtyDepart = coneSQtyDepart;
+    }
+
+    public int getConeBQtyDepart() {
+        return coneBQtyDepart;
+    }
+
+    public void setConeBQtyDepart(int coneBQtyDepart) {
+        this.coneBQtyDepart = coneBQtyDepart;
+    }
+
+    public int getNuttyQtyDepart() {
+        return nuttyQtyDepart;
+    }
+
+    public void setNuttyQtyDepart(int nuttyQtyDepart) {
+        this.nuttyQtyDepart = nuttyQtyDepart;
+    }
+
+    public int getKeshaerQtyDepart() {
+        return keshaerQtyDepart;
+    }
+
+    public void setKeshaerQtyDepart(int keshaerQtyDepart) {
+        this.keshaerQtyDepart = keshaerQtyDepart;
+    }
+
+    public int getBonanzaQtyDepart() {
+        return bonanzaQtyDepart;
+    }
+
+    public void setBonanzaQtyDepart(int bonanzaQtyDepart) {
+        this.bonanzaQtyDepart = bonanzaQtyDepart;
+    }
+
+    public int getFamilyQtyDepart() {
+        return familyQtyDepart;
+    }
+
+    public void setFamilyQtyDepart(int familyQtyDepart) {
+        this.familyQtyDepart = familyQtyDepart;
+    }
+
+    public int getKacchaQtyReturn() {
+        return kacchaQtyReturn;
+    }
+
+    public void setKacchaQtyReturn(int kacchaQtyReturn) {
+        this.kacchaQtyReturn = kacchaQtyReturn;
+    }
+
+    public int getLitchiQtyReturn() {
+        return litchiQtyReturn;
+    }
+
+    public void setLitchiQtyReturn(int litchiQtyReturn) {
+        this.litchiQtyReturn = litchiQtyReturn;
+    }
+
+    public int getStrawQtyReturn() {
+        return strawQtyReturn;
+    }
+
+    public void setStrawQtyReturn(int strawQtyReturn) {
+        this.strawQtyReturn = strawQtyReturn;
+    }
+
+    public int getColaQtyReturn() {
+        return colaQtyReturn;
+    }
+
+    public void setColaQtyReturn(int colaQtyReturn) {
+        this.colaQtyReturn = colaQtyReturn;
+    }
+
+    public int getPineQtyReturn() {
+        return pineQtyReturn;
+    }
+
+    public void setPineQtyReturn(int pineQtyReturn) {
+        this.pineQtyReturn = pineQtyReturn;
+    }
+
+    public int getOrangeQtyReturn() {
+        return orangeQtyReturn;
+    }
+
+    public void setOrangeQtyReturn(int orangeQtyReturn) {
+        this.orangeQtyReturn = orangeQtyReturn;
+    }
+
+    public int getMangoQtyReturn() {
+        return mangoQtyReturn;
+    }
+
+    public void setMangoQtyReturn(int mangoQtyReturn) {
+        this.mangoQtyReturn = mangoQtyReturn;
+    }
+
+    public int getCupSQtyReturn() {
+        return cupSQtyReturn;
+    }
+
+    public void setCupSQtyReturn(int cupSQtyReturn) {
+        this.cupSQtyReturn = cupSQtyReturn;
+    }
+
+    public int getCupBQtyReturn() {
+        return cupBQtyReturn;
+    }
+
+    public void setCupBQtyReturn(int cupBQtyReturn) {
+        this.cupBQtyReturn = cupBQtyReturn;
+    }
+
+    public int getChocoSQtyReturn() {
+        return chocoSQtyReturn;
+    }
+
+    public void setChocoSQtyReturn(int chocoSQtyReturn) {
+        this.chocoSQtyReturn = chocoSQtyReturn;
+    }
+
+    public int getChocoBQtyReturn() {
+        return chocoBQtyReturn;
+    }
+
+    public void setChocoBQtyReturn(int chocoBQtyReturn) {
+        this.chocoBQtyReturn = chocoBQtyReturn;
+    }
+
+    public int getMatkaQtyReturn() {
+        return matkaQtyReturn;
+    }
+
+    public void setMatkaQtyReturn(int matkaQtyReturn) {
+        this.matkaQtyReturn = matkaQtyReturn;
+    }
+
+    public int getConeSQtyReturn() {
+        return coneSQtyReturn;
+    }
+
+    public void setConeSQtyReturn(int coneSQtyReturn) {
+        this.coneSQtyReturn = coneSQtyReturn;
+    }
+
+    public int getConeBQtyReturn() {
+        return coneBQtyReturn;
+    }
+
+    public void setConeBQtyReturn(int coneBQtyReturn) {
+        this.coneBQtyReturn = coneBQtyReturn;
+    }
+
+    public int getNuttyQtyReturn() {
+        return nuttyQtyReturn;
+    }
+
+    public void setNuttyQtyReturn(int nuttyQtyReturn) {
+        this.nuttyQtyReturn = nuttyQtyReturn;
+    }
+
+    public int getKeshaerQtyReturn() {
+        return keshaerQtyReturn;
+    }
+
+    public void setKeshaerQtyReturn(int keshaerQtyReturn) {
+        this.keshaerQtyReturn = keshaerQtyReturn;
+    }
+
+    public int getBonanzaQtyReturn() {
+        return bonanzaQtyReturn;
+    }
+
+    public void setBonanzaQtyReturn(int bonanzaQtyReturn) {
+        this.bonanzaQtyReturn = bonanzaQtyReturn;
+    }
+
+    public int getFamilyQtyReturn() {
+        return familyQtyReturn;
+    }
+
+    public void setFamilyQtyReturn(int familyQtyReturn) {
+        this.familyQtyReturn = familyQtyReturn;
+    }
+
+    int kacchaQtyReturn;
+    int litchiQtyReturn;
+    int strawQtyReturn;
+    int colaQtyReturn;
+    int pineQtyReturn;
+    int orangeQtyReturn;
+    int mangoQtyReturn;
+    int cupSQtyReturn;
+    int cupBQtyReturn;
+    int chocoSQtyReturn;
+    int chocoBQtyReturn;
+    int matkaQtyReturn;
+    int coneSQtyReturn;
+    int coneBQtyReturn;
+    int nuttyQtyReturn;
+    int keshaerQtyReturn;
+    int bonanzaQtyReturn;
+    int familyQtyReturn;
+    int family2QtyReturn;
+
+
+    int commision_percentage;
+
+    public int getCommision_percentage() {
+        return commision_percentage;
+    }
+
+    public void setCommision_percentage(int commision_percentage) {
+        this.commision_percentage = commision_percentage;
+    }
+
+    String custName;
+    long invoiceNo;
+    String address;
+
+    public DetailsObjVendor() {
+    }
+
+    int kacchaPrice;
+    int litchiPrice;
+    int strawPrice;
+    int colaPrice;
+    int pinePrice;
+    int orangePrice;
+    int mangoPrice;
+    int cupSPrice;
+    int cupBPrice;
+
+    public int getKacchaPrice() {
+        return kacchaPrice;
+    }
+
+    public void setKacchaPrice(int kacchaPrice) {
+        this.kacchaPrice = kacchaPrice;
+    }
+
+    public int getLitchiPrice() {
+        return litchiPrice;
+    }
+
+    public void setLitchiPrice(int litchiPrice) {
+        this.litchiPrice = litchiPrice;
+    }
+
+    public int getStrawPrice() {
+        return strawPrice;
+    }
+
+    public void setStrawPrice(int strawPrice) {
+        this.strawPrice = strawPrice;
+    }
+
+    public int getColaPrice() {
+        return colaPrice;
+    }
+
+    public void setColaPrice(int colaPrice) {
+        this.colaPrice = colaPrice;
+    }
+
+    public int getPinePrice() {
+        return pinePrice;
+    }
+
+    public void setPinePrice(int pinePrice) {
+        this.pinePrice = pinePrice;
+    }
+
+    public int getOrangePrice() {
+        return orangePrice;
+    }
+
+    public void setOrangePrice(int orangePrice) {
+        this.orangePrice = orangePrice;
+    }
+
+    public int getMangoPrice() {
+        return mangoPrice;
+    }
+
+    public void setMangoPrice(int mangoPrice) {
+        this.mangoPrice = mangoPrice;
+    }
+
+    public int getCupSPrice() {
+        return cupSPrice;
+    }
+
+    public void setCupSPrice(int cupSPrice) {
+        this.cupSPrice = cupSPrice;
+    }
+
+    public int getCupBPrice() {
+        return cupBPrice;
+    }
+
+    public void setCupBPrice(int cupBPrice) {
+        this.cupBPrice = cupBPrice;
+    }
+
+    public int getChocoSPrice() {
+        return chocoSPrice;
+    }
+
+    public void setChocoSPrice(int chocoSPrice) {
+        this.chocoSPrice = chocoSPrice;
+    }
+
+    public int getChocoBPrice() {
+        return chocoBPrice;
+    }
+
+    public void setChocoBPrice(int chocoBPrice) {
+        this.chocoBPrice = chocoBPrice;
+    }
+
+    public int getMatkaPrice() {
+        return matkaPrice;
+    }
+
+    public void setMatkaPrice(int matkaPrice) {
+        this.matkaPrice = matkaPrice;
+    }
+
+    public int getConeSPrice() {
+        return coneSPrice;
+    }
+
+    public void setConeSPrice(int coneSPrice) {
+        this.coneSPrice = coneSPrice;
+    }
+
+    public int getConeBPrice() {
+        return coneBPrice;
+    }
+
+    public void setConeBPrice(int coneBPrice) {
+        this.coneBPrice = coneBPrice;
+    }
+
+    public int getNuttyPrice() {
+        return nuttyPrice;
+    }
+
+    public void setNuttyPrice(int nuttyPrice) {
+        this.nuttyPrice = nuttyPrice;
+    }
+
+    public int getKeshaerPrice() {
+        return keshaerPrice;
+    }
+
+    public void setKeshaerPrice(int keshaerPrice) {
+        this.keshaerPrice = keshaerPrice;
+    }
+
+    public int getBonanzaPrice() {
+        return bonanzaPrice;
+    }
+
+    public void setBonanzaPrice(int bonanzaPrice) {
+        this.bonanzaPrice = bonanzaPrice;
+    }
+
+    public int getFamilyPrice() {
+        return familyPrice;
+    }
+
+    public void setFamilyPrice(int familyPrice) {
+        this.familyPrice = familyPrice;
+    }
+
+    int chocoSPrice;
+    int chocoBPrice;
+    int matkaPrice;
+    int coneSPrice;
+    int coneBPrice;
+    int nuttyPrice;
+    int keshaerPrice;
+    int bonanzaPrice;
+    int familyPrice;
+    int family2Price;
+
+
+
+
+    Double commision;
+    Double total;
+    Double dues;
+    long date;
+    public long getDate() {
+        return date;
+    }
+
+    public int getFamily2QtyDepart() {
+        return family2QtyDepart;
+    }
+
+    public void setFamily2QtyDepart(int family2QtyDepart) {
+        this.family2QtyDepart = family2QtyDepart;
+    }
+
+    public int getFamily2QtyReturn() {
+        return family2QtyReturn;
+    }
+
+    public void setFamily2QtyReturn(int family2QtyReturn) {
+        this.family2QtyReturn = family2QtyReturn;
+    }
+
+    public int getFamily2Price() {
+        return family2Price;
+    }
+
+    public void setFamily2Price(int family2Price) {
+        this.family2Price = family2Price;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
+
+    public String getCustName() {
+        return custName;
+    }
+
+    public void setCustName(String custName) {
+        this.custName = custName;
+    }
+
+    public long getInvoiceNo() {
+        return invoiceNo;
+    }
+
+    public void setInvoiceNo(long invoiceNo) {
+        this.invoiceNo = invoiceNo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+
+    public Double getCommision() {
+        return commision;
+    }
+
+    public void setCommision(Double commision) {
+        this.commision = commision;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public Double getDues() {
+        return dues;
+    }
+
+    public void setDues(Double dues) {
+        this.dues = dues;
+    }
+
+
+
+
+}
