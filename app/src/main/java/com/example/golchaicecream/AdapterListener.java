fdaeb5620ac9941314415031347b5c547298edbf
kush-raj-nit061 <kushpvt1@gmail.com>
2023-09-15 18:33:46 +0530

    launching0.1

--- app/src/main/java/com/example/golchaicecream/AdapterListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.golchaicecream;
-
-import com.example.golchaicecream.Room.Users;
-
-public interface AdapterListener {
-    void onUpdate(Users users);
-    void onDelete(int id,int pos);
-}
